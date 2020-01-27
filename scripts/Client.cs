6e0420adc76677dfb02595be64717e28c744178b
leoxshn <leoxshn@gmail.com>
2020-01-27 22:15:02 +0100

    made it work with potassium2

--- scripts/Client.cs
+++ scripts/Client.cs
@@ -1,155 +1,39 @@
 using System;
-using System.Net;
+using System.IO;
 using System.Net.Sockets;
-using System.Threading;
 using System.Text;
-using Godot;
 
-// State object for receiving data from remote device.  
-public class StateObject {
-    // Client socket.  
-    public Socket workSocket = null;
-    // Size of receive buffer.  
-    public const int BufferSize = 256;
-    // Receive buffer.  
-    public byte[] buffer = new byte[BufferSize];
-    // Received data string.  
-    public StringBuilder sb = new StringBuilder();
-}
+public class Client {
 
-public class AsynchronousClient {
-    // The port number for the remote device.  
-    private const int port = 2512;
+    private static Stream stream;
+    private static TcpClient client = new TcpClient();
 
-    // ManualResetEvent instances signal completion.  
-    private static ManualResetEvent connectDone = new ManualResetEvent(false);
-    private static ManualResetEvent sendDone = new ManualResetEvent(false);
-    private static ManualResetEvent receiveDone = new ManualResetEvent(false);
-
-    // The response from the remote device.
-    private static String response = String.Empty;
-
-    public static void StartClient() {
+    public static void init() {
         try {
-            // Establish the remote endpoint for the socket.  
-            // The name of the   
-            // remote device is "host.contoso.com".  
-            IPHostEntry ipHostInfo = Dns.GetHostEntry("83.59.187.91");
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+            client.Connect("81.36.136.84", 2512);
+            stream = client.GetStream();
 
-            // Create a TCP/IP socket.  
-            Socket client = new Socket(
-                ipAddress.AddressFamily,
-                SocketType.Stream,
-                ProtocolType.Tcp
-            );
+            var str = "join&74567854697b'm'540697b509'569&stupid human";
 
-            // Connect to the remote endpoint.  
-            client.BeginConnect(
-                remoteEP,
-                new AsyncCallback(ConnectCallback),
-                client
-            );
-            connectDone.WaitOne();
+            Console.WriteLine("Transmitting...");
+            send(str);
+            Console.WriteLine("Transmitted");
 
-            // Send test data to the remote device.  
-            Send(client, "This is a test<EOF>");
-            sendDone.WaitOne();
+            Console.WriteLine("Press ENTER to stop");
+            Console.ReadKey();
 
-            // Receive the response from the remote device.  
-            Receive(client);
-            receiveDone.WaitOne();
-
-            // Write the response to the console.  
-            GD.Print("Response received : {0}", response);
-
-            // Release the socket.  
-            client.Shutdown(SocketShutdown.Both);
-            client.Close();
-        } catch (Exception e) { GD.Print(e.ToString()); }
+        } catch (Exception e) { Console.WriteLine("error: " + e.StackTrace); }
     }
 
-    private static void ConnectCallback(IAsyncResult ar) {
-        try {
-            // Retrieve the socket from the state object.  
-            Socket client = (Socket)ar.AsyncState;
-
-            // Complete the connection.  
-            client.EndConnect(ar);
-
-            GD.Print("Socket connected to {0}",
-                client.RemoteEndPoint.ToString());
-
-            // Signal that the connection has been made.  
-            connectDone.Set();
-        } catch (Exception e) { GD.Print(e.ToString()); }
+    public static void clean(string packet) {
+        stream.Close();
+        client.Close();
     }
 
-    private static void Receive(Socket client) {
-        try {
-            // Create the state object.  
-            StateObject state = new StateObject();
-            state.workSocket = client;
-            // Begin receiving the data from the remote device.  
-            client.BeginReceive(
-                state.buffer, 0,
-                StateObject.BufferSize, 0,
-                new AsyncCallback(ReceiveCallback), state
-            );
-        } catch (Exception e) { GD.Print(e.ToString()); }
-    }
-
-    private static void ReceiveCallback(IAsyncResult ar) {
-        try {
-            // Retrieve the state object and the client socket   
-            // from the asynchronous state object.  
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket client = state.workSocket;
-
-            // Read data from the remote device.  
-            int bytesRead = client.EndReceive(ar);
-
-            if (bytesRead > 0) {
-                // There might be more data, so store the data received so far.  
-                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
-
-                // Get the rest of the data.  
-                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                    new AsyncCallback(ReceiveCallback), state);
-            } else {
-                // All the data has arrived; put it in response.  
-                if (state.sb.Length > 1) {
-                    response = state.sb.ToString();
-                }
-                // Signal that all bytes have been received.  
-                receiveDone.Set();
-            }
-        } catch (Exception e) { GD.Print(e); }
-    }
-
-    private static void Send(Socket client, String data) {
-        // Convert the string data to byte data using ASCII encoding.  
-        byte[] byteData = Encoding.ASCII.GetBytes(data);
-
-        // Begin sending the data to the remote device.  
-        client.BeginSend(
-            byteData, 0,
-            byteData.Length, 0,
-            new AsyncCallback(SendCallback),
-            client
-        );
-    }
-
-    private static void SendCallback(IAsyncResult ar) {
-        try {
-            // Retrieve the socket from the state object.  
-            Socket client = (Socket)ar.AsyncState;
-            // Complete sending the data to the remote device.  
-            int bytesSent = client.EndSend(ar);
-            GD.Print("Sent {0} bytes to server.", bytesSent);
-            // Signal that all bytes have been sent.  
-            sendDone.Set();
-        } catch (Exception e) { GD.Print(e); }
+    public static void send(string packet) {
+        byte[] ba = Encoding.ASCII.GetBytes(packet);
+        stream.Write(ba, 0, ba.Length);
+        stream.WriteByte(0x0A);
+        stream.Flush();
     }
 }
--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,6 +3,6 @@ using Godot;
 public class Main : Spatial {
 
     public override void _Ready() {
-        //AsynchronousClient.StartClient();
+        Client.init();
     }
 }
